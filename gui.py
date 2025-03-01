55a88f1d636c819aa0d0506d75b53d33b62bfe08
Teresa <tto46@csu.fullerton.edu>
2025-02-28 20:40:53 -0800

    Removed the user's input

--- gui.py
+++ gui.py
@@ -14,18 +14,23 @@ def random_list(size):
     return [random.randint(1, 9999) for _ in range(size)]
 
 def get_input(): 
-    global sorted_data # we need to call it back for linear search
+    # we need to call it back for linear search
+    global sorted_data
 
     # ensures that the entries are only integers greater than 0
     try:
-        num_data_points = int(data_entry.get())
+        num_data_points = int(array_size_entry.get())
 
-        if num_data_points < 0: # prompt error if entered value is less than zero
+        # prompt error if entered value is less than zero
+        if num_data_points < 0:
             raise ValueError
         
-        data = random_list(num_data_points)  # generate random values
-    except ValueError:  # prompts error if a letter is detected 
-        result.insert("1.0", "Please only enter numbers greater than zero.\nThe search key can only be an integer.\n")
+        # generate random values
+        data = random_list(num_data_points)
+
+    # prompts error if a letter is detected 
+    except ValueError:
+        result.insert("1.0", "Please only enter positive values.\n")
         return
 
     # Check which sorting algorithm is selected
@@ -37,7 +42,8 @@ def get_input():
     execution_times = []
     algorithms = []
 
-    for algo in selected_algorithms:    # for selected algos, sort data and print results
+    # for selected algos, sort data and print results
+    for algo in selected_algorithms:
         start_time = time.time_ns()
         if algo == "Bubble Sort":
             sorted_data = sort_algos.bubble_sort(data)  
@@ -49,7 +55,9 @@ def get_input():
             sorted_data = sort_algos.lsd_radix_sort(data)
        
         end_time = time.time_ns()
-        execution_time = end_time - start_time # calculate effiency of each search algo
+
+        # calculate effiency of each search algo
+        execution_time = end_time - start_time
 
         execution_times.append(execution_time)
         algorithms.append(algo)
@@ -70,9 +78,11 @@ def get_input():
     nanoseconds_lists = [np.linspace(0, time, num_frames) for time in execution_times]
     def animate(i):
         
-        # Update the height of each bar based on the current frame
+        # update the height of each bar based on the current frame
         for j, bar in enumerate(bars[0]):
-            bar.set_height(nanoseconds_lists[j][i])  # Update bar height for each algorith
+
+            # update bar height for each algorith
+            bar.set_height(nanoseconds_lists[j][i])
         
     plt.xlabel("Sorting Algorithm")
     plt.ylabel("Execution Time (nanoseconds)")
@@ -84,21 +94,27 @@ def get_input():
 
 def lin_search():
     try:
-        user_search = int(search_entry.get()) # take user entry for linear search
+        # take user entry for linear search
+        user_search = int(search_entry.get())
+
+        # checks that the search value is positive only
+        if user_search < 0:
+            raise ValueError
     except ValueError:
-        result.cinsert("1.0", "Please enter a valid integer.\n")
+        result.insert("1.0", "Please enter a positive integer.\n")
         return
     search_result = sort_algos.linear_search(sorted_data, user_search)
 
     if search_result != -1:
-        result.insert("1.0", f"{user_search} found at index {search_result}\n")
+        result.insert("1.0", f"{user_search} is found at index {search_result}\n")
     else:
-        result.insert("1.0", f"{user_search} not found in the data\n")
+        result.insert("1.0", f"{user_search} is not found in the data\n")
         
 # clears everything
 def reset_data():
-    data_entry.delete(0, 'end')
+    array_size_entry.delete(0, 'end')
     search_entry.delete(0, 'end')
+    input_entry.delete(0, 'end')
 
     for clear_checkbox in checkbox_frame.winfo_children():
         clear_checkbox.deselect()
@@ -109,32 +125,21 @@ def reset_data():
 def select_all():
     for all_checkbox in checkbox_frame.winfo_children():
         all_checkbox.select()
-
+ 
 root = tk.Tk()
 root.title("Group Project # 1 Algorithm Analyzer Tool")
 
-# needs to do something similar to his enter comma-separated inputs like the video
+# generates an n-sized array of random number based on the array size entered
+array_size = tk.Label(root, text="Enter array size:")
+array_size.grid(row = 2, column = 0, rowspan=1)
 
-# this just generates an array of random number based on the array size entered
-# label for data points input
-data_points = tk.Label(root, text="Enter number of data points:")
-data_points.grid(row = 0, column = 0, rowspan=1)
-
-# creates text box for data points input
-data_entry = tk.Entry(root, width=30)
-data_entry.grid(row = 1, column = 0, padx = 25)
-
-# label prompting user search
-user_search = tk.Label(root, text="Search for: ")
-user_search.grid(row = 5, column = 0)
-
-# creates text box for search input
-search_entry = tk.Entry(root, width=30)
-search_entry.grid(row = 6, column = 0)
+# creates text box for n-sized array entry
+array_size_entry = tk.Entry(root, width=30)
+array_size_entry.grid(row = 3, column = 0, padx = 25)
 
 # frame to hold the checkboxes
 checkbox_frame = tk.Frame(root)
-checkbox_frame.grid(row = 2, column = 0)
+checkbox_frame.grid(row = 4, column = 0)
 
 # sorting algorithms options
 algorithms = ["Bubble Sort", "Merge Sort", "Quick Sort", "Radix Sort"]
@@ -143,30 +148,38 @@ print(check_vars)
 
 # select all button
 select_all_button = tk.Button(root, text="Select All", command=select_all)
-select_all_button.grid(row=3, column=0)
+select_all_button.grid(row=5, column=0)
 
 # submit button
 submit_data_button = tk.Button(root, text="Submit", command=get_input)
-submit_data_button.grid(row = 4, column = 0)
-
-# submit button
-search_button = tk.Button(root, text="Submit", command=lin_search)
-search_button.grid(row = 7, column = 0)
+submit_data_button.grid(row = 6, column = 0)
 
 # reset button
 reset_data_button = tk.Button(root, text="Reset", command=reset_data)
-reset_data_button.grid(row = 8, column = 0)
+reset_data_button.grid(row = 7, column = 0)
+
+# label prompting user search
+user_search = tk.Label(root, text="Search for: (Linear Search)")
+user_search.grid(row = 8, column = 0)
+
+# creates text box for search input
+search_entry = tk.Entry(root, width=30)
+search_entry.grid(row = 9, column = 0)
+
+# linear search submit button
+search_button = tk.Button(root, text="Submit", command=lin_search)
+search_button.grid(row = 10, column = 0, pady=15)
 
 # create checkboxes
 for algo, var in check_vars.items():
     chk = tk.Checkbutton(checkbox_frame, text=algo, variable=var)
     chk.pack(anchor="w")
 
-# Label to display results
+# label to display results
 result = tk.Text(root, height=30, width=60)
 result.grid(row = 0, column = 1, rowspan = 8, pady = 40, padx = 25)
 
-# Run the Tkinter event loop
+# run the Tkinter event loop
 root.mainloop()
 
 # tkinter
